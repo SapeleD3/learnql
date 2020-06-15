a0d2c95683f650330a269760c25495c148c16fd4
sapeled3 <oyinkuromosesvictor@outlook.com>
2020-06-15 23:15:36 +0100

    setup prisma

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,36 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2020: true,
-    node: true,
-  },
-  extends: [
-    "airbnb-base",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended",
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: "module",
-  },
-  plugins: ["@typescript-eslint"],
-  rules: {
-    "import/extensions": [
-      "error",
-      "ignorePackages",
-      {
-        js: "never",
-        jsx: "never",
-        ts: "never",
-        tsx: "never",
-      },
-    ],
-  },
-  "no-console": "off",
-  "prettier/prettier": "error",
+	env: {
+		browser: true,
+		es2020: true,
+		node: true,
+	},
+	extends: [
+		"airbnb-base",
+		"plugin:import/errors",
+		"plugin:import/warnings",
+		"plugin:import/typescript",
+		"prettier/@typescript-eslint",
+		"plugin:prettier/recommended",
+	],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaVersion: 2018,
+		sourceType: "module",
+	},
+	plugins: ["@typescript-eslint"],
+	rules: {
+		"import/prefer-default-export": "off",
+		"prettier/prettier": "error",
+		"no-console": "off",
+		"import/extensions": [
+			"error",
+			"ignorePackages",
+			{
+				js: "never",
+				jsx: "never",
+				ts: "never",
+				tsx: "never",
+			},
+		],
+	},
 };
--- src/typeDefs.ts
+++ src/typeDefs.ts
@@ -1,7 +1,15 @@
 import { gql } from "apollo-server-express";
 
 export const typeDefs = gql`
+	type Tests {
+		id: String
+		message: String
+	}
 	type Query {
 		hello: String!
+		getTests: [Tests]
+	}
+	type Mutation {
+		postTest: Tests
 	}
 `;
